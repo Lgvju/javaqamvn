4ce82bd4feea5a0fb922c75c535d8a100b18e061
OEM <gurleva_ludmila@mail.ru>
2023-06-25 19:55:29 +0300

    Rename percent to Percent

--- src/main/java/ru/myproject/javaqa/Main.java
+++ src/main/java/ru/myproject/javaqa/Main.java
@@ -1,9 +1,10 @@
 package ru.myproject.javaqa;
+
 public class Main {
     public static void main(String[] args) {
         BonusService service = new BonusService();
         int expected = 150;
-        long actual = service.calculate(5000,true);
-        System.out.println(expected+ "==?==" +actual);
+        long actual = service.calculate(5000, true);
+        System.out.println(expected + "==?==" + actual);
     }
 }
--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -35,6 +35,7 @@ public class BonusServiceTest {
         // производим проверку (сравниваем ожидаемый и фактический):
         Assertions.assertEquals(expected, actual);
     }
+
     @Test
     void shouldCalculateForUnRegisteredAndUnderLimit() {
         BonusService service = new BonusService();
