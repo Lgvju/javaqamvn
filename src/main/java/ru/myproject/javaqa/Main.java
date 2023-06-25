6f27320f6476236f8332ec4d2abbb9a7d681ea5f
OEM <gurleva_ludmila@mail.ru>
2023-06-25 18:52:43 +0300

    changes

---
+++ src/main/java/ru/myproject/javaqa/Main.java
@@ -0,0 +1,9 @@
+package ru.myproject.javaqa;
+public class Main {
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
+        int expected = 150;
+        long actual = service.calculate(5000,true);
+        System.out.println(expected+ "==?==" +actual);
+    }
+}
